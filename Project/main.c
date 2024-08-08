3d76972d67ebaa7e701c42a6f6fd9442708a84b7
zhibaixiao <yongshengli666@gmail.com>
2024-08-08 20:19:21 +0800

    LED流水灯

--- Project/main.c
+++ Project/main.c
@@ -1,27 +1,24 @@
 #include "RTE_Components.h"
 #include "stm32f10x_gpio.h"
-#include <stdio.h>
 #include CMSIS_device_header
 #include "Delay.h"
 int main() {
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
   GPIO_InitTypeDef GPIO_InitStructure;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
 
   // GPIO_ResetBits(GPIOA, GPIO_Pin_0);
 
   for (;;) {
-    GPIO_ResetBits(GPIOA, GPIO_Pin_0); // 点亮
-    Delay_ms(500);
-    GPIO_SetBits(GPIOA, GPIO_Pin_0); // 熄灭
-    Delay_ms(500);
 
-    GPIO_WriteBit(GPIOA, GPIO_Pin_0, 1);   // 点亮LED
-    Delay_ms(500);                               // 延时
-    GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET); // 熄灭LED
+    GPIO_Write(GPIOA, ~0x0001);
+    Delay_ms(500);
+    GPIO_Write(GPIOA, ~0x0002);
+    Delay_ms(500);
+    GPIO_Write(GPIOA, ~0x0004);
     Delay_ms(500);
   }
 }
