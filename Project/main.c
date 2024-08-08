41d88e17623353461488ff9630131eab7beadaed
zhibaixiao <yongshengli666@gmail.com>
2024-08-08 12:04:34 +0800

    点亮A0处LED

--- Project/main.c
+++ Project/main.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include CMSIS_device_header
 
+
 int main() {
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
     for (;;) {
     }
 }
